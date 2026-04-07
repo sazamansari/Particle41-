1b1ae213218bc1d8463c47f6800084e978156d8d
Md Shadab Azam ANsari <shadabazama@gmail.com>
2026-04-07 23:02:07 +0530

    RESET VPC

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,14 +7,12 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 }
-
 
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
@@ -23,25 +21,21 @@ resource "aws_subnet" "public_2" {
   map_public_ip_on_launch = true
 }
 
-
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-south-1a"
 }
 
-
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 }
-
 
 resource "aws_route" "internet_access" {
   route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
-
 
 resource "aws_route_table_association" "public_assoc_1" {
   subnet_id      = aws_subnet.public.id
